524eaeffc0345c74a45a613025b60b1a4e0cc979
Yulia Garifulina <hackwrenchh@gmail.com>
2016-08-19 23:59:30 +0300

    Implement countdown

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,4 @@
 'use strict';
-var btn = document.getElementsByClassName("start-btn")[0];
-btn.addEventListener('click', entryPoint);
 
 var userSettings = document.getElementsByTagName('input');
 
@@ -11,15 +9,15 @@ function getTime(inputList) {
     })
     timeList.forEach(function(item) {
         try {
-            if (isNaN(parseFloat(item)) || isFinite(item) || item < 1) {
+            if (isNaN(parseFloat(item)) || !isFinite(item) || item < 1) {
                 throw new TimeFormatException('Invalid time format');
             }
         } catch (err) {
             if (err instanceof TimeFormatException) {
-                alert('Invalid format. Time is set up to default state')
-                return [5, 25];
-            };
-        }
+                alert('Invalid format. Time is set up to the default state')
+                timeList = [5, 25];
+            }
+        };
     });
     return timeList;
 }
@@ -32,24 +30,54 @@ function TimeFormatException(message) {
 function Clock(fullTime) {
     var breakLength = fullTime[0];
     var sessionLength = fullTime[1];
-    var timer = function(time) {
-        setInterval(function() {
-            time = time - 1;
+    var countdown;
+    var time;
+    var count = 0;
+    var pause = false;
+    var timer = function() {
+      if (!pause) {
+        countdown = setInterval(function() {
+            time -= 1;
+            count++;
             console.log(time);
         }, 1000);
+      }
+      else {
+        // time = time - count - 2;
+        clearInterval(countdown);
+      }
     }
-    this.startSession = function() {
-        var time = sessionLength * 60;
-        timer(time);
+    this.start = function() {
+        if (!time) time = sessionLength * 60;
+        timer();
     }
     this.startBreak = function() {
-        var time = breakLength * 60;
-        timer(time);
+        time = breakLength * 60;
+        timer();
+    }
+    this.isPause = function() {
+      return pause;
+    }
+    this.toggle = function() {
+      if (pause) { pause = false }
+      else { pause = true;}
+        console.log('pause changes to ', pause);
     }
 }
 
+var pomodoro = new Clock(getTime(userSettings));
+
+var btn = document.getElementsByClassName("start-btn")[0];
+btn.addEventListener('click', entryPoint);
+
 //Event handler entry point after btn click event has been occured
 function entryPoint() {
-    var pomodoro = new Clock(getTime(userSettings));
-    pomodoro.startSession();
+  //preventdeafult
+  pomodoro.start();
+
+    if (!pomodoro.isPause()) {
+        pomodoro.toggle();
+    } else {
+        pomodoro.toggle();
+    }
 }
